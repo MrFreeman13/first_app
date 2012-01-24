c93f0ecf90565c41419d35c2a26a42a507a48fef
Yevhenii Yevtushenko <freeman.zhenia@gmail.com>
2012-01-25 01:13:29 +0200

    after spork

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
+  describe " GET 'home' " do
     it "should be successful" do
       get 'home'
       response.should be_success
