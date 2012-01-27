64b5ec381b9a7756b034a7c91babebc55f746053
root <root@freeman13-pc.(none)>
2012-01-27 23:37:22 +0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 FirstApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/home',     :to => 'pages#home'
+
+  match '/signup',  :to => 'users#new'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
-  root :to => 'pages#home'
+  root              :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
